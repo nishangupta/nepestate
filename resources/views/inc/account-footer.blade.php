30050a3f992fe555b4e985366c48f5ed15808eb9
nisangupta <nishangupta@icloud.com>
2020-08-15 15:04:20 +0545

    included footer from inc/account-footer

---
+++ resources/views/inc/account-footer.blade.php
@@ -0,0 +1,11 @@
+<div class="footer">
+  <p class="footer-text">Zillow Group is committed to ensuring digital accessibility for individuals with
+    disabilities. We are continuously working to improve the accessibility of our web experience for
+    everyone, and we welcome feedback and accommodation requests. If you wish to report an issue or seek an
+    accommodation, please <a href="#">contact us.</a></p>
+  <p class="footer-text mt-4">
+    Copyright © 2020 Trulia, LLC. All rights reserved. Equal Housing Opportunity. Have a Question? Visit our
+    Help Center to find the answer.
+  </p>
+</div>
+</div>
