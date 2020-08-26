08a5113076bf20a737e9e50f87ca96f222562cfc
nisangupta <nishangupta@icloud.com>
2020-08-26 15:35:24 +0545

    slugifed userPropertyController@index

--- app/Http/Controllers/UserPropertyController.php
+++ app/Http/Controllers/UserPropertyController.php
@@ -29,7 +29,7 @@ class UserPropertyController extends Controller
           return date('d/m/Y h:i A', strtotime($row->created_at));
         })
         ->addColumn('actions', function ($row) {
-          $btns = '<a class="btn btn-info  btn-sm btn-block btn-sm-block" href="/account/property-listings/' . $row->id . '" ><i class="fas fa-eye"></i> View</a>
+          $btns = '<a class="btn btn-info  btn-sm btn-block btn-sm-block" href="' . $row->path() . '" ><i class="fas fa-eye"></i> View</a>
           <a class="btn btn-primary btn-sm btn-block btn-sm-block" href="/account/property-listings/' . $row->id . '/edit"><i class="fas fa-pen-square"></i> Edit</a>
           <button class="btn btn-danger btn-block btn-sm btn-sm-block" onclick="tableDelete(' . $row->id . ')" ><i class="fas fa-trash-alt"></i> Delete</button>';
           return $btns;
@@ -85,7 +85,7 @@ class UserPropertyController extends Controller
 
   public function destroy(Property $property)
   {
-    if ($property->user_id !== auth()->user()->id) {
+    if ($property->user_id == auth()->user()->id) {
       if ($property->delete()) {
         return ['data' => true, 'msg' => 'Record deleted!'];
       } else {
