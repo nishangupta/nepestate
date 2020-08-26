f89d8679eff4b5b681a8a55db5a12b4f276f94ef
nisangupta <nishangupta@icloud.com>
2020-08-26 14:27:15 +0545

    created account crud test

---
+++ tests/Feature/AccountCrudTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class AccountCrudTest extends TestCase
+{
+    use RefreshDatabase;
+    /** @test */
+    public function a_user_can_be_created()
+    {
+        $this->withoutExceptionHandling();
+
+        $response = $this->post('/account/login', [
+            'email' => 'admin@admin.com',
+            'password' => 'password'
+        ]);
+
+        $this->assertCount(1, User::all());
+    }
+}
